78e142b886c2ddf47557df7388d7f629018864a2
gretdain <oleg_vsh@umail.ru>
2025-09-29 10:57:40 +0300

    sync

--- js/accordion.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  $( function() {
-    $( "#accordion" ).accordion({
-      active: false,
-      heightStyle: "content",
-      collapsible: true
-    });
-  });
-})
